88f3a73ad16b7f36f3b76dc46bd16c92cda919bd
Gabin Favergeat <ton.emailexemple.com>
2026-04-28 16:35:25 -0400

    GSU landing page route

---
+++ pages/gsu.js
@@ -0,0 +1,12 @@
+﻿import fs from 'fs';
+import path from 'path';
+
+export default function GSU() { return null; }
+
+export async function getServerSideProps({ res }) {
+  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'gsu', 'index.html'), 'utf8');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.write(html);
+  res.end();
+  return { props: {} };
+}
